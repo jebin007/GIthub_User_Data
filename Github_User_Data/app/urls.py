881df0bab553bc1bc07021f2f283f092dd593bf9
jebin007 <jevinsubedi@gmail.com>
2017-09-27 03:19:43 -0500

    Created Profile View To Use Github API and Url for The User DrkSephy

--- Github_User_Data/app/urls.py
+++ Github_User_Data/app/urls.py
@@ -7,5 +7,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^test/$', views.test, name='test'),
+    url(r'^profile/$', views.profile, name='profile'),
 ]
 
--- Github_User_Data/app/views.py
+++ Github_User_Data/app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, HttpResponse
+import requests
+import json
 
 #Create your views here.
 
@@ -7,3 +9,8 @@ def index(request):
 
 def test(request):
     return HttpResponse('My Second View!')
+
+def profile(request):
+    req = requests.get('https://api.github.com/users/DrkSephy')
+    content = req.text
+    return HttpResponse(content)
